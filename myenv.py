ca6c90ec66816b66b498e409c22af2f0f31b87a0
柴士童 <chaihahaha@qq.com>
2020-10-21 19:37:56 +0800

    mysterious policy network update formula

--- myenv.py
+++ myenv.py
@@ -17,7 +17,9 @@ N_STATES = 36
 N_ACTIONS = 15
 MEMORY_CAPACITY = 1000
 EPSILON = 0.9 
-GAMMA = 0.9
+γ = 0.9
+αw = 1
+αθ = 1
 TARGET_REPLACE_ITER = 100
 STATES_SHAPE = [3,15,15,3]
 LR = 1e-3
@@ -38,107 +40,96 @@ class Net(nn.Module):
         self.fc1.weight.data.normal_(0, 0.1)   # initialization
         self.out = nn.Linear(50, N_ACTIONS)
         self.out.weight.data.normal_(0, 0.1)   # initialization
+        self.tanh = nn.Tanh()
 
     def forward(self, x):
         x = self.fc1(x)
         x = F.relu(x)
-        actions_value = self.out(x)
+        actions_value = 4 * self.tanh(self.out(x))
         return actions_value
         
 class QNet(nn.Module):
     def __init__(self):
         super(QNet, self).__init__()
-        self.fc1 = nn.Linear(N_ACTIONS, 50)
+        self.fc1 = nn.Linear(N_STATES, 50)
+        self.fc2 = nn.Linear(N_ACTIONS, 50)
         self.fc1.weight.data.normal_(0, 0.1)   # initialization
-        self.out = nn.Linear(50, 1)
+        self.fc2.weight.data.normal_(0, 0.1)   # initialization
+        self.out = nn.Linear(100, 1)
         self.out.weight.data.normal_(0, 0.1)   # initialization
+        self.tanh = nn.Tanh()
 
-    def forward(self, x):
-        x = self.fc1(x)
-        x = F.relu(x)
+    def forward(self, x1, x2):
+        x1 = self.fc1(x1)
+        x2 = self.fc2(x2)
+        x = F.relu(torch.stack([x1,x2],1).view(-1,100))
         q = self.out(x)
+        q = self.tanh(q)
         return q
 
 
-class DQN(object):
-    def __init__(self, actions):
+class DAC(Policy):
+    def __init__(self, env, states, actions):
+        super(DAC, self).__init__(states, actions)
+        self.env = env
+        self.states = states
         self.actions = actions
-        self.eval_net, self.target_net, self.qnet = Net(), Net(), QNet()
-
-        self.learn_step_counter = 0                                     # for target updating
-        self.memory_counter = 0                                         # for storing memory
-        self.memory = np.zeros((MEMORY_CAPACITY, N_STATES * 2 + N_ACTIONS + 1))     # initialize memory
-        self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=LR)
-        self.loss_func = nn.MSELoss()
-
-    def predict(self, x, to_numpy):
-        x = torch.FloatTensor(x)
-        x = torch.unsqueeze(x, 0)
-        # input only one sample
-        if np.random.uniform() < EPSILON:   # greedy
-            action = self.eval_net.forward(x).detach().numpy().reshape(N_ACTIONS)
-        else:   # random
-            action = self.actions.space.sample()
-        return action
-
-    def store_transition(self, s, a, r, s_):
-        transition = np.hstack((s, a, [r], s_))
-        # replace the old memory with new memory
-        index = self.memory_counter % MEMORY_CAPACITY
-        self.memory[index, :] = transition
-        self.memory_counter += 1
+        self.action_data = None
+        self.μ, self.Q = Net(), QNet()
 
     def learn(self):
-        # target parameter update
-        if self.learn_step_counter % TARGET_REPLACE_ITER == 0:
-            self.target_net.load_state_dict(self.eval_net.state_dict())
-        self.learn_step_counter += 1
-
-        # sample batch transitions
-        sample_index = np.random.choice(MEMORY_CAPACITY, BATCH_SIZE)
-        b_memory = self.memory[sample_index, :]
-        b_s = torch.FloatTensor(b_memory[:, :N_STATES])
-        b_a = torch.LongTensor(b_memory[:, N_STATES:N_STATES+N_ACTIONS].astype(int))
-        b_r = torch.FloatTensor(b_memory[:, N_STATES+N_ACTIONS:N_STATES+N_ACTIONS+1])
-        b_s_ = torch.FloatTensor(b_memory[:, -N_STATES:])
-
-        # q_eval w.r.t the action in experience
-        q_eval = self.qnet(self.eval_net(b_s))  # shape (batch, 1)
-        q_next = self.qnet(self.target_net(b_s_).detach())     # detach from graph, don't backpropagate
+        # get state $s_t$
+        st = self.states.vec_data
+        st = torch.tensor(st, requires_grad=True, dtype=torch.float)
         
-        q_target = b_r + GAMMA * q_next   # shape (batch, 1)
-        loss = self.loss_func(q_eval, q_target)
-
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
+        # get action $a_t$ with policy μ and $s_t$
+        at = self.μ(st)
         
-class RandomPolicy(Policy):
-    class RandomModel(object):
-        def __init__(self, actions, seed=None):
-            self.seed = seed
-            self.actions = actions
-        @property
-        def seed(self):
-            return self._seed
-        @seed.setter
-        def seed(self, seed):
-            if seed is not None:
-                np.random.seed(seed)
-            self._seed = seed
-        def reset(self):
-            pass
+        # copy $a_t$ for gradient
+        at2 = at.data
+        at2.requires_grad = True
+        
+        # ε-greedy
+        if np.random.uniform() < EPSILON:
+            a_env = at.data.numpy()
+        else:
+            a_env = self.env.action.space.sample()
 
-        def predict(self, state=None, to_numpy=True):
-            space = self.actions.space
-            return space.sample() 
+        # apply action
+        self.set_action_data(a_env)
+        self.actions()
+        
+        # step in environment to get next state $s_{t+1}$, reward $r_t$
+        st_, rt, done, info = self.env.step(a_env)
+        
+        # cast to tensor
+        st_ = torch.tensor(st_, dtype=torch.float)
+        
+        # get action $a_{t+1}$ with policy μ and $s_{t+1}$
+        at_ = self.μ(st_)
+        
+        # get action values Q and Q_
+        Q = self.Q(st,at2)
+        Q_ = self.Q(st_,at_)
+        
+        # δ_t = r_t + γ * Q(s_{t+1}, a_{t+1}) - Q(s_t, a_t)
+        δt = rt + γ * Q_[0] - Q[0]
+        
+        self.Q.zero_grad()
+        Q.backward()
+        for w in self.Q.parameters():
+            # w_{t+1} = w_t + α_w * δ_t * ∇_w Q(s_t, a_t)
+            w.data = w.data + αw * δt * w.grad
+        
+        # ∇_a Q(s_t, a_t)|_{a=μ(s)}
+        ΔaQ = at2.grad
 
-    def __init__(self, state, action, rate=1, seed=None, *args, **kwargs):
-        model = DQN(action)
-        super(RandomPolicy, self).__init__(state, action, model=model, rate=rate, *args, **kwargs)
-
-    def sample(self, state=None):
-        return self.act(state)
+        self.μ.zero_grad()
+        at.backward(ΔaQ)
+        for θ in self.μ.parameters():
+            # θ_{t+1} = θ_t + α_θ * ∇_θ μ(s_t) ∇_a Q
+            θ.data = θ.data + αθ * θ.grad
+        return rt
 
 if __name__=="__main__":
     sim = prl.simulators.Bullet()
@@ -147,7 +138,6 @@ if __name__=="__main__":
     manipulator = world.load_robot('wam')
     states = BasePositionState(manipulator) + JointPositionState(manipulator) + JointVelocityState(manipulator) + PositionState(box,world)
     action = JointPositionAction(manipulator)
-    policy = RandomPolicy(states,action)
     reward = TerminalReward(HasPickedAndPlacedCondition(manipulator,box,world))
     env = Env(world, states, rewards=reward,actions=action)
     
@@ -155,13 +145,8 @@ if __name__=="__main__":
     num_steps=10000
     env.reset()
     env.render()
+    dac = DAC(env, states, action)
     for t in count():
         if t>=num_steps:
             break
-        obs = env.state.vec_data
-        action = policy.act(obs)
-        obs_, reward, done, info = env.step(action)
-        print(reward)
-        policy.model.store_transition(obs, action, reward, obs_)
-        if policy.model.memory_counter > MEMORY_CAPACITY:
-            policy.model.learn()
+        print(dac.learn())
