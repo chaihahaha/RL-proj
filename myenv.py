62ceef4bb36c2796da81f7c64f0af006bb16e8c5
柴士童 <chaihahaha@qq.com>
2020-10-23 18:48:41 +0800

    losses, targetnet and memory

--- myenv.py
+++ myenv.py
@@ -1,9 +1,4 @@
 import pyrobolearn as prl
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from itertools import count
 from pyrobolearn.envs import Env
 from pyrobolearn.policies import Policy
 from pyrobolearn.rewards.terminal_rewards import TerminalReward
@@ -14,18 +9,24 @@ from pyrobolearn.actions.robot_actions.joint_actions import JointPositionAction
 from pyrobolearn.tasks.reinforcement import RLTask
 from pyrobolearn.rewards.reward import Reward
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numpy as np
+import time
+from itertools import count
 
 N_STATES = 36
 N_ACTIONS = 15
 MEMORY_CAPACITY = 1000
 EPSILON = 0.9
 γ = 0.9
-αw = 1e-3
-αθ = 1e-3
 TARGET_REPLACE_ITER = 100
 STATES_SHAPE = [3,15,15,3]
 LR = 1e-3
 BATCH_SIZE = 20
+tau = 0.5
 
 class HasPickedAndLiftedCondition(TerminalCondition):
     def __init__(self, robot, box, world):
@@ -71,23 +72,35 @@ class μNet(nn.Module):
         self.bounds = bounds
         self.fc1 = nn.Linear(N_STATES, 50)
         self.fc1.weight.data.normal_(0, 0.1)   # initialization
-        self.fc2 = nn.Linear(50, 100)
+        self.fc2 = nn.Linear(50, 50)
         self.fc2.weight.data.normal_(0, 0.1)   # initialization
-        self.out = nn.Linear(100, N_ACTIONS)
+        self.fc3 = nn.Linear(50, 50)
+        self.fc3.weight.data.normal_(0, 0.1)   # initialization
+        self.fc4 = nn.Linear(50, 50)
+        self.fc4.weight.data.normal_(0, 0.1)   # initialization
+        self.fc5 = nn.Linear(50, 50)
+        self.fc5.weight.data.normal_(0, 0.1)   # initialization
+        self.out = nn.Linear(50, N_ACTIONS)
         self.out.weight.data.normal_(0, 0.1)   # initialization
         self.tanh = nn.Tanh()
-        self.act = nn.LeakyReLU(0.2, inplace=True)
+        self.act = nn.LeakyReLU(0.2, inplace=False)
 
     def forward(self, x):
         x = self.fc1(x)
         x = self.act(x)
         x = self.fc2(x)
         x = self.act(x)
+        x = self.fc3(x)
+        x = self.act(x)
+        x = self.fc4(x)
+        x = self.act(x)
+        x = self.fc5(x)
+        x = self.act(x)
         x = self.out(x)
         actions = []
         for a in range(N_ACTIONS):
-            actions.append(self.clip(x[a],self.bounds[a,0],self.bounds[a,1]))
-        return torch.stack(actions,axis=0).view(N_ACTIONS)
+            actions.append(self.clip(x[:,a],self.bounds[a,0],self.bounds[a,1]))
+        return torch.stack(actions,axis=0).view(-1,N_ACTIONS)
         
     def clip(self, x, x_min, x_max):
         return x_min + (x_max-x_min)*(self.tanh(x)+1)/2
@@ -99,20 +112,42 @@ class QNet(nn.Module):
         self.fc1.weight.data.normal_(0, 0.1)   # initialization
         self.fc2 = nn.Linear(N_ACTIONS, 50)
         self.fc2.weight.data.normal_(0, 0.1)   # initialization
-        self.out1 = nn.Linear(100, 100)
+        self.out1 = nn.Linear(100, 50)
         self.out1.weight.data.normal_(0, 0.1)   # initialization
-        self.out2 = nn.Linear(100, 1)
+        self.out2 = nn.Linear(50, 50)
         self.out2.weight.data.normal_(0, 0.1)   # initialization
+        self.out3 = nn.Linear(50, 50)
+        self.out3.weight.data.normal_(0, 0.1)   # initialization
+        self.out4 = nn.Linear(50, 1)
+        self.out4.weight.data.normal_(0, 0.1)   # initialization
         self.tanh = nn.Tanh()
-        self.act = nn.LeakyReLU(0.2, inplace=True)
+        self.act = nn.LeakyReLU(0.2, inplace=False)
 
     def forward(self, x1, x2):
         x1 = self.act(self.fc1(x1))
         x2 = self.act(self.fc2(x2))
-        x = self.act(torch.stack([x1,x2],1).view(-1,100))
-        x = self.act(self.out1(x))
-        x = self.act(self.out2(x))
-        return x
+        x3 = torch.stack([x1,x2],1).view(-1,100)
+        x4 = self.act(x3)
+        x5 = self.out1(x4)
+        x6 = self.act(x5)
+        x7 = self.out2(x6)
+        x8 = self.act(x7)
+        x9 = self.out3(x8)
+        x10 = self.act(x9)
+        x11 = self.out4(x10)
+        out = self.act(x11)
+        return out
+
+class ACNet(nn.Module):
+    def __init__(self, bounds):
+        super(ACNet, self).__init__()
+        self.μ = μNet(bounds)
+        self.Q = QNet()
+    
+    def forward(self, s):
+        self.a = self.μ(s)
+        q = self.Q(s,self.a)
+        return q
 
 # Deterministic Actor Critic
 class DDPG_AC(Policy):
@@ -123,28 +158,29 @@ class DDPG_AC(Policy):
         self.actions = actions
         self.action_data = None
         self.device = device
-        self.μ, self.Q = μNet(bounds), QNet()
-        self.μ.to(device)
-        self.Q.to(device)
-        self.memory = []
+        self.ac = ACNet(bounds)
+        self.ac.to(device)
+        trainableμ = list(filter(lambda p: p.requires_grad, self.ac.μ.parameters()))
+        self.optimμ = torch.optim.Adam(trainableμ, lr=LR, betas=(0.9, 0.95), eps=1e-6, weight_decay=1e-5)
+        trainableQ = list(filter(lambda p: p.requires_grad, self.ac.Q.parameters()))
+        self.optimQ = torch.optim.Adam(trainableQ, lr=LR, betas=(0.9, 0.95), eps=1e-6, weight_decay=1e-5)
+        self.ac_tar = ACNet(bounds)
+        self.ac_tar.to(device)
+        
+        self.memory = torch.zeros((MEMORY_CAPACITY,N_STATES*2+N_ACTIONS+1),device=self.device)
+        self.cnt = 0
 
     def learn(self):
         # get state $s_t$
         st = self.states.vec_data
         st = torch.tensor(st, requires_grad=True, dtype=torch.float,device=self.device)
         
-        # get action $a_t$ with policy μ and $s_t$
-        at = self.μ(st)
+        st1 = st.unsqueeze(0)
+        at = self.ac.μ(st1).detach()
+        Q = self.ac.Q(st1,at)
         
-        # copy $a_t$ for gradient
-        at2 = at.data
-        at2.requires_grad = True
-        
-        # ε-greedy
-        if np.random.uniform() < EPSILON:
-            a_env = at.data.cpu().numpy()
-        else:
-            a_env = self.env.action.space.sample()
+        # add noise
+        a_env = (at.data.cpu() + torch.rand(at.data.shape)).numpy()[0]
 
         # apply action
         self.set_action_data(a_env)
@@ -152,45 +188,57 @@ class DDPG_AC(Policy):
         
         # step in environment to get next state $s_{t+1}$, reward $r_t$
         st_, rt, done, info = self.env.step(a_env)
-        manipulator.get_end_effector_ids()[-1]
         # cast to tensor
         st_ = torch.tensor(st_, dtype=torch.float,device=self.device)
         
-        # get action $a_{t+1}$ with policy μ and $s_{t+1}$
-        at_ = self.μ(st_)
+        # keep (st, at, rt, st_) in memory
+        index = self.cnt % MEMORY_CAPACITY
+        self.memory[index,:N_STATES] = st
+        self.memory[index,N_STATES:N_STATES+N_ACTIONS] = at
+        self.memory[index,-N_STATES-1] = rt
+        self.memory[index,-N_STATES:] = st_
         
-        # get action values Q and Q_
-        Q = self.Q(st,at2)
-        Q_ = self.Q(st_,at_)
-        #print(Q,Q_)
+        st_1 = st_.unsqueeze(0)
+        at_ = self.ac_tar.μ(st_1)
+        Q_ = self.ac_tar.Q(st_1,at_).detach()
         
         # δ_t = r_t + γ * Q(s_{t+1}, a_{t+1}) - Q(s_t, a_t)
-        δt = rt + γ * Q_[0] - Q[0]
+        δt = rt + γ * Q_ - Q
+        loss1 = δt**2
+        loss1.backward(retain_graph=True)
+        self.optimQ.step()
+        self.optimQ.zero_grad()
+        #print(loss1,flush=True)
         
-        self.Q.zero_grad()
-        Q.backward()
-        for w in self.Q.parameters():
-            # w_{t+1} = w_t + α_w * δ_t * ∇_w Q(s_t, a_t)
-            w.data = w.data + αw * δt * w.grad
-        #print(sum([torch.sum(i) for i in self.μ.parameters()]))
-        # ∇_a Q(s_t, a_t)|_{a=μ(s)}
-        ΔaQ = at2.grad
-
-        self.μ.zero_grad()
-        at.backward(ΔaQ)
-        for θ in self.μ.parameters():
-            # θ_{t+1} = θ_t + α_θ * ∇_θ μ(s_t) ∇_a Q
-            θ.data = θ.data + αθ * θ.grad
-            
-        # keep (st, at, st_, at_) in memory
-        #self.memory.append((st, at, st_, at_))
+        right = self.cnt if self.cnt<MEMORY_CAPACITY else MEMORY_CAPACITY
+        Qm = self.ac(self.memory[:right,:N_STATES])
+        loss2 = -torch.sum(Qm)
+        self.optimμ.step()
+        self.optimμ.zero_grad()
+        #print(loss2,flush=True)
+        
+        # update target network
+        p = self.ac.named_parameters()
+        p_tar = self.ac_tar.named_parameters()
+        d_tar = dict(p_tar)
+        for name, param in p:
+            d_tar[name].data = tau*param.data + (1-tau) * d_tar[name].data
+        
+        self.cnt += 1
         return rt, done
+    
+    def save(self, filename):
+        torch.save({'net':self.ac.state_dict()}, filename)
+    
+    def load(self, filename):
+        state_dicts = torch.load(filename)
+        self.ac.load_state_dict(state_dicts['net'])
 
 def success(reward):
     return reward >= -0.5
 
 if __name__=="__main__":
-    sim = prl.simulators.Bullet(render=False)
+    sim = prl.simulators.Bullet(render=True)
     world = prl.worlds.BasicWorld(sim)
     box = world.load_box(position=(0.5,0,0.2),dimensions=(0.1,0.1,0.1),mass=0.1,color=[0,0,1,1])
     manipulator = world.load_robot('wam')
@@ -199,17 +247,20 @@ if __name__=="__main__":
     bounds = action.bounds()
     r_cond = HasTouchedCondition(manipulator,box,world,0.5)
     t_cond = HasTouchedCondition(manipulator,box,world,0.5)
-    #reward = TerminalReward(r_cond,subreward=-1,final_reward=0)
-    reward = ApproachBoxReward(manipulator,box,world)
+    reward = TerminalReward(r_cond,subreward=-1,final_reward=0)
+    #reward = ApproachBoxReward(manipulator,box,world)
     env = Env(world, states, rewards=reward,actions=action,terminal_conditions=t_cond)
     
     env.reset()
     ddpg = DDPG_AC(env, states, action, "cuda")
-    num_episodes = 1000
+    num_episodes = 3000
+    save_freq = 200
     t_episode = 100
     n_samples = 100
     n_success = 0
-    for i in range(num_episodes):
+    s_reward = 0
+    tik = time.time()
+    for i in range(1,num_episodes+1):
         # randomly reset robot joint and box position
         manipulator.reset_joint_states(np.random.uniform(-1,1,(15)))
         world.move_object(box,[np.random.uniform(-1,1),np.random.uniform(-1,1),0.2])
@@ -219,13 +270,19 @@ if __name__=="__main__":
             reward, done = ddpg.learn()
             if done:
                 break
+        n_success += 1 if done else 0
+        s_reward += reward
+        print("SUCCESS" if done else "FAIL",flush=True)
         
         # collect statistics of #n_samples results
-        if i%n_samples==0 and i>0:
-            print("Statistics {}-{}\tSuccess rate: {:.4f}".format(i-n_samples,i,n_success/n_samples),flush=True)
+        if i%n_samples==0:
+            tok = time.time()
+            print("{}\tSuc rate: {:.4f}\tAvg reward: {:.4}\tTime: {}".format(i/n_samples,n_success/n_samples,s_reward/n_samples,tok-tik),flush=True)
             n_success = 0
-        n_success += 1 if done else 0
-        print("SUCCESS" if done else "FAIL",flush=True)
+            s_reward = 0
+            tik = time.time()
+        if i%save_freq==0:
+            ddpg.save("ddpg.ckpt")
 #    for i in count():
 #        obs,reward,done,info = env.step()
 #        print(reward)
