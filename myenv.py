4509499d5246cb641347bcef2a9cdc53dfc569ef
柴士童 <chaihahaha@qq.com>
2020-10-24 22:44:48 +0800

    TD3

--- myenv.py
+++ myenv.py
@@ -18,9 +18,9 @@ import time
 from itertools import count
 
 N_ACTIONS = 15
-MEMORY_CAPACITY = 1000
-EPSILON = 0.1
-γ = 0.9
+MEMORY_CAPACITY = 5120
+EPSILON = 0.4
+γ = 0.994
 LR = 1e-3
 BATCH_SIZE = 512
 TRAIN_FREQ = 20
@@ -28,6 +28,10 @@ TARGET_REPLACE_ITER = TRAIN_FREQ
 tau = 0.9
 t_episode = 100
 
+def layer_norm(layer, std=1.0, bias_const=0.0):
+    torch.nn.init.orthogonal_(layer.weight, std)
+    torch.nn.init.constant_(layer.bias, bias_const)
+    
 class MyCameraState(SensorState):
     def __init__(self, camera):
         self._sensor = camera
@@ -88,20 +92,26 @@ class μNet(nn.Module):
     def __init__(self, bounds):
         super(μNet, self).__init__()
         self.bounds = bounds
-        self.fc1 = nn.Linear(N_STATES, 50)
-        self.fc1.weight.data.normal_(0, 0.1)   # initialization
-        self.fc2 = nn.Linear(50, 100)
-        self.fc2.weight.data.normal_(0, 0.1)   # initialization
-        self.fc3 = nn.Linear(100, 50)
-        self.fc3.weight.data.normal_(0, 0.1)   # initialization
-        self.fc4 = nn.Linear(50, 200)
-        self.fc4.weight.data.normal_(0, 0.1)   # initialization
-        self.fc5 = nn.Linear(200, 50)
-        self.fc5.weight.data.normal_(0, 0.1)   # initialization
-        self.out = nn.Linear(50, N_ACTIONS)
-        self.out.weight.data.normal_(0, 0.1)   # initialization
+        self.fc1 = nn.Linear(N_STATES, 200)
+        self.fc1.weight.data.normal_(0, 1e-5)   # initialization
+        self.fc2 = nn.Linear(200, 200)
+        self.fc2.weight.data.normal_(0, 1e-5)   # initialization
+        self.fc3 = nn.Linear(200, 200)
+        self.fc3.weight.data.normal_(0, 1e-5)   # initialization
+        self.fc4 = nn.Linear(200, 200)
+        self.fc4.weight.data.normal_(0, 1e-5)   # initialization
+        self.fc5 = nn.Linear(200, 200)
+        self.fc5.weight.data.normal_(0, 1e-5)   # initialization
+        self.out = nn.Linear(200, N_ACTIONS)
+        self.out.weight.data.normal_(0, 1e-5)   # initialization
         self.tanh = nn.Tanh()
         self.act = nn.LeakyReLU(0.2, inplace=True)
+        
+        layer_norm(self.fc1)
+        layer_norm(self.fc2)
+        layer_norm(self.fc3)
+        layer_norm(self.fc4)
+        layer_norm(self.fc5)
 
     def forward(self, x):
         x = self.act(self.fc1(x))
@@ -120,20 +130,26 @@ class μNet(nn.Module):
 class QNet(nn.Module):
     def __init__(self):
         super(QNet, self).__init__()
-        self.fc1 = nn.Linear(N_STATES, 50)
-        self.fc1.weight.data.normal_(0, 0.1)   # initialization
-        self.fc2 = nn.Linear(N_ACTIONS, 50)
-        self.fc2.weight.data.normal_(0, 0.1)   # initialization
-        self.out1 = nn.Linear(100, 50)
-        self.out1.weight.data.normal_(0, 0.1)   # initialization
-        self.out2 = nn.Linear(50, 100)
-        self.out2.weight.data.normal_(0, 0.1)   # initialization
-        self.out3 = nn.Linear(100, 50)
-        self.out3.weight.data.normal_(0, 0.1)   # initialization
-        self.out = nn.Linear(50, 1)
-        self.out.weight.data.normal_(0, 0.1)   # initialization
+        self.fc1 = nn.Linear(N_STATES, 200)
+        self.fc1.weight.data.normal_(0, 1e-5)   # initialization
+        self.fc2 = nn.Linear(N_ACTIONS, 100)
+        self.fc2.weight.data.normal_(0, 1e-5)   # initialization
+        self.out1 = nn.Linear(300, 300)
+        self.out1.weight.data.normal_(0, 1e-5)   # initialization
+        self.out2 = nn.Linear(300, 300)
+        self.out2.weight.data.normal_(0, 1e-5)   # initialization
+        self.out3 = nn.Linear(300, 300)
+        self.out3.weight.data.normal_(0, 1e-5)   # initialization
+        self.out = nn.Linear(300, 1)
+        self.out.weight.data.normal_(0, 1e-5)   # initialization
         self.tanh = nn.Tanh()
         self.act = nn.LeakyReLU(0.2, inplace=True)
+        layer_norm(self.fc1)
+        layer_norm(self.fc2)
+        layer_norm(self.out1)
+        layer_norm(self.out2)
+        layer_norm(self.out3)
+        layer_norm(self.out)
 
     def forward(self, x1, x2):
         x1 = self.act(self.fc1(x1))
@@ -145,36 +161,34 @@ class QNet(nn.Module):
         out = self.out(x)
         return out
 
-class ACNet(nn.Module):
-    def __init__(self, bounds):
-        super(ACNet, self).__init__()
-        self.μ = μNet(bounds)
-        self.Q = QNet()
-    
-    def forward(self, s):
-        self.a = self.μ(s)
-        q = self.Q(s,self.a)
-        return q
-
-# Deterministic Actor Critic
-class DDPG_AC(Policy):
+class TD3(Policy):
     def __init__(self, env, states, actions,robot, device):
-        super(DDPG_AC, self).__init__(states, actions)
+        super(TD3, self).__init__(states, actions)
         self.robot = robot
         self.env = env
         self.states = states
         self.actions = actions
         self.action_data = None
         self.device = device
-        self.ac = ACNet(actions.bounds())
-        self.ac.to(device)
-        trainableμ = list(filter(lambda p: p.requires_grad, self.ac.μ.parameters()))
+        self.μ = μNet(actions.bounds())
+        self.μ.to(device)
+        self.Q1, self.Q2 = QNet(), QNet()
+        self.Q1.to(self.device)
+        self.Q2.to(self.device)
+        trainableμ = list(filter(lambda p: p.requires_grad, self.μ.parameters()))
         self.optimμ = torch.optim.Adam(trainableμ, lr=LR, betas=(0.9, 0.95), eps=1e-6, weight_decay=1e-5)
-        trainableQ = list(filter(lambda p: p.requires_grad, self.ac.Q.parameters()))
-        self.optimQ = torch.optim.Adam(trainableQ, lr=LR, betas=(0.9, 0.95), eps=1e-6, weight_decay=1e-5)
-        self.ac_tar = ACNet(actions.bounds())
-        self.ac_tar.to(device)
-        
+        trainableQ1 = list(filter(lambda p: p.requires_grad, self.Q1.parameters()))
+        self.optimQ1 = torch.optim.Adam(trainableQ1, lr=LR, betas=(0.9, 0.95), eps=1e-6, weight_decay=1e-5)
+        trainableQ2 = list(filter(lambda p: p.requires_grad, self.Q2.parameters()))
+        self.optimQ2 = torch.optim.Adam(trainableQ2, lr=LR, betas=(0.9, 0.95), eps=1e-6, weight_decay=1e-5)
+        self.μ_tar = μNet(actions.bounds())
+        self.μ_tar.to(device)
+        self.Q1_tar, self.Q2_tar = QNet(), QNet()
+        self.Q1_tar.to(self.device)
+        self.Q2_tar.to(self.device)
+        self.μ_tar.load_state_dict(self.μ.state_dict())
+        self.Q1_tar.load_state_dict(self.Q1.state_dict())
+        self.Q2_tar.load_state_dict(self.Q2.state_dict())
         self.memory = torch.zeros((MEMORY_CAPACITY,N_STATES*2+N_ACTIONS+1),device=self.device)
         self.cnt = 0
 
@@ -183,7 +197,8 @@ class DDPG_AC(Policy):
         st = self.states.vec_torch_data.float().to(self.device).unsqueeze(0)
         
         # compute action with actor μ
-        at = self.ac.μ(st) + EPSILON*torch.randn((st.shape[0],N_ACTIONS),device=self.device)
+        at = self.μ(st)
+        at += EPSILON*torch.randn(at.shape,device=self.device)
         
         # cast to numpy
         at_np = (at.data.cpu()).numpy()[0]
@@ -220,11 +235,13 @@ class DDPG_AC(Policy):
         # update target network
         if  (self.cnt+1) % TARGET_REPLACE_ITER == 0:
             # update target network
-            p = self.ac.named_parameters()
-            p_tar = self.ac_tar.named_parameters()
-            d_tar = dict(p_tar)
-            for name, param in p:
-                d_tar[name].data = tau*param.data + (1-tau) * d_tar[name].data
+            update_pairs = [(self.μ, self.μ_tar), (self.Q1, self.Q1_tar), (self.Q2, self.Q2_tar)]
+            for i, i_tar in update_pairs:
+                p = i.named_parameters()
+                p_tar = i_tar.named_parameters()
+                d_tar = dict(p_tar)
+                for name, param in p:
+                    d_tar[name].data = tau*param.data + (1-tau) * d_tar[name].data
         
         # HER replace goal
         if timeout and (not done):
@@ -257,30 +274,45 @@ class DDPG_AC(Policy):
         return rt, done
         
     def train(self, si, ai, ri, si_):
-        Qm = self.ac(si)
+        Qm = self.Q1(si, self.μ(si))
         lossμ = -torch.mean(Qm)
         lossμ.backward(retain_graph=True)
         self.optimμ.step()
         self.optimμ.zero_grad()
 
-        Q = self.ac.Q(si,ai)
-        Q_ = self.ac_tar(si_).detach()
+        ai_ = self.μ_tar(si_).detach()
+        ai_ += EPSILON*torch.randn(ai_.shape,device=self.device)
+        Q1_ = self.Q1_tar(si_,ai_).detach()
+        Q2_ = self.Q2_tar(si_,ai_).detach()
+        y = ri + γ * torch.min(Q1_, Q2_)
         
-        # δ_t = r_t + γ * Q(s_{t+1}, a_{t+1}) - Q(s_t, a_t)
-        δt = ri + γ * Q_ - Q
-        lossQ = torch.mean(δt**2)
-        lossQ.backward()
-        self.optimQ.step()
-        self.optimQ.zero_grad()
+        Q1 = self.Q1(si, ai)
+        lossQ1 = torch.mean((y-Q1)**2)
+        lossQ1.backward()
+        self.optimQ1.step()
+        self.optimQ1.zero_grad()
+        
+        Q2 = self.Q2(si, ai)
+        lossQ2 = torch.mean((y-Q2)**2)
+        lossQ2.backward()
+        self.optimQ2.step()
+        self.optimQ2.zero_grad()
 
             
     
     def save(self, filename):
-        torch.save({'net':self.ac.state_dict()}, filename)
+        torch.save({'μ':self.μ_tar.state_dict(),
+                    'Q1':self.Q1_tar.state_dict(),
+                    'Q2':self.Q2_tar.state_dict()}, filename)
     
     def load(self, filename):
         state_dicts = torch.load(filename)
-        self.ac.load_state_dict(state_dicts['net'])
+        self.μ.load_state_dict(state_dicts['μ'])
+        self.Q1.load_state_dict(state_dicts['Q1'])
+        self.Q2.load_state_dict(state_dicts['Q2'])
+        self.μ_tar.load_state_dict(state_dicts['μ'])
+        self.Q1_tar.load_state_dict(state_dicts['Q1'])
+        self.Q2_tar.load_state_dict(state_dicts['Q2'])
 
 def success(reward):
     return reward >= -0.5
@@ -304,8 +336,8 @@ if __name__=="__main__":
     env = Env(world, states, rewards=reward,actions=action,terminal_conditions=t_cond)
     
     env.reset()
-    ddpg = DDPG_AC(env, states, action,manipulator, "cuda")
-    #ddpg.load("ddpg.ckpt")
+    td3 = TD3(env, states, action,manipulator, "cuda")
+    #td3.load("td3.ckpt")
     num_episodes = 30000
     save_freq = 200
     
@@ -321,7 +353,7 @@ if __name__=="__main__":
         # run an episode
         for t in range(t_episode):
             timeout = t==t_episode-1
-            reward, done = ddpg.learn(timeout)
+            reward, done = td3.learn(timeout)
             if done:
                 break
         n_success += 1 if done else 0
@@ -336,7 +368,7 @@ if __name__=="__main__":
             s_reward = 0
             tik = time.time()
         if i%save_freq==0:
-            ddpg.save("ddpg.ckpt")
+            td3.save("td3.ckpt")
 #    for i in count():
 #        obs,reward,done,info = env.step()
 #        print(reward)
