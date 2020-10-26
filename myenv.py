e28af6e4f1f3bc9ec89b89a8ad54cb699c767b9a
柴士童 <chaihahaha@qq.com>
2020-10-26 14:00:10 +0800

    real reward for HER

--- myenv.py
+++ myenv.py
@@ -18,55 +18,39 @@ import time
 from itertools import count
 
 N_ACTIONS = 15
-MEMORY_CAPACITY = 5120
+MEMORY_CAPACITY = int(1e5)
 NOISE_EPSILON = 0.1
-RAND_EPSILON = 0.3
-ACTION_L2 = 0.1
+RAND_EPSILON = 0.1
+ACTION_L2 = 1
 γ = 0.999
 LR = 1e-3
 BATCH_SIZE = 512
 TRAIN_FREQ = 20
 TARGET_REPLACE_ITER = TRAIN_FREQ * 2
-tau = 0.9
+polyak = 0.95
 t_episode = 100
 
-class HasPickedAndLiftedCondition(TerminalCondition):
-    def __init__(self, robot, box, world):
-        self.robot = robot
-        self.box = box
-        self.world = world
-    def check(self):
-        x,y,z = self.world.get_body_position(self.box)
-        return z>0.5
+def picked_and_lifted_reward(box_pos):
+    assert len(box_pos) == 3
+    x,y,z = box_pos
+    return z>0.5
         
-class HasTouchedCondition(TerminalCondition):
-    def __init__(self, robot, box, world, radius):
-        self.robot = robot
-        self.box = box
-        self.world = world
-        self.radius = radius
-    def check(self):
-        end_effector = self.robot.get_end_effector_ids()[-1]
-        x1,y1,z1 = self.robot.get_link_world_positions(end_effector)
-        x2,y2,z2 = self.world.get_body_position(self.box)
-        dx,dy,dz = x1-x2,y1-y2,z1-z2
-        return dx**2 + dy**2 + dz**2 <=self.radius**2
+def touched_reward(end_effector_pos, box_pos):
+    assert len(end_effector_pos) == 3
+    assert len(box_pos) == 3
+    x1,y1,z1 = end_effector_pos
+    x2,y2,z2 = box_pos
+    dx,dy,dz = x1-x2,y1-y2,z1-z2
+    return 0. if dx**2 + dy**2 + dz**2 <=0.5**2 else -1.
         
-class ApproachBoxReward(Reward):
-    def __init__(self, robot, box, world, range=(-np.infty,0)):
-        self.robot = robot
-        self.box = box
-        self.world = world
-        self.range = range
-        
-    def _compute(self):
-        """Compute the reward."""
-        end_effector = self.robot.get_end_effector_ids()[-1]
-        x1,y1,z1 = self.robot.get_link_world_positions(end_effector)
-        x2,y2,z2 = self.world.get_body_position(self.box)
-        dx,dy,dz = x1-x2,y1-y2,z1-z2
-        self.value = -(dx**2+dy**2+dz**2)**0.5
-        return self.value
+def approach_box_reward(end_effector_pos, box_pos):
+    assert len(end_effector_pos) == 3
+    assert len(box_pos) == 3
+    x1,y1,z1 = end_effector_pos
+    x2,y2,z2 = box_pos
+    dx,dy,dz = x1-x2,y1-y2,z1-z2
+    value = -(dx**2+dy**2+dz**2)**0.5
+    return value
         
 class μNet(nn.Module):
     def __init__(self, bounds):
@@ -100,8 +84,8 @@ class μNet(nn.Module):
         x = self.act(self.fc4(x))
         x = self.act(self.fc5(x))
         x = self.out(x)
-        for i in range(N_ACTIONS):
-            x[:,i] = self.clip(x[:,i], self.bounds[i,0],self.bounds[i,1])
+#        for i in range(N_ACTIONS):
+#            x[:,i] = self.clip(x[:,i], self.bounds[i,0],self.bounds[i,1])
         return x
         
     def clip(self, x, x_min, x_max):
@@ -143,9 +127,10 @@ class QNet(nn.Module):
         return out
 
 class TD3(Policy):
-    def __init__(self, env, states, actions,robot, device):
+    def __init__(self, env, states, actions, robot, reward, device):
         super(TD3, self).__init__(states, actions)
         self.robot = robot
+        self.reward = reward
         self.env = env
         self.states = states
         self.actions = actions
@@ -157,11 +142,11 @@ class TD3(Policy):
         self.Q1.to(self.device)
         self.Q2.to(self.device)
         trainableμ = list(filter(lambda p: p.requires_grad, self.μ.parameters()))
-        self.optimμ = torch.optim.Adam(trainableμ, lr=LR, betas=(0.9, 0.95), eps=1e-6, weight_decay=1e-5)
+        self.optimμ = torch.optim.Adam(trainableμ, lr=LR)
         trainableQ1 = list(filter(lambda p: p.requires_grad, self.Q1.parameters()))
-        self.optimQ1 = torch.optim.Adam(trainableQ1, lr=LR, betas=(0.9, 0.95), eps=1e-6, weight_decay=1e-5)
+        self.optimQ1 = torch.optim.Adam(trainableQ1, lr=LR)
         trainableQ2 = list(filter(lambda p: p.requires_grad, self.Q2.parameters()))
-        self.optimQ2 = torch.optim.Adam(trainableQ2, lr=LR, betas=(0.9, 0.95), eps=1e-6, weight_decay=1e-5)
+        self.optimQ2 = torch.optim.Adam(trainableQ2, lr=LR)
         with torch.no_grad():
             self.μ_tar = μNet(actions.bounds())
             self.Q1_tar, self.Q2_tar = QNet(), QNet()
@@ -192,7 +177,9 @@ class TD3(Policy):
         self.actions()
         
         # step in environment to get next state $s_{t+1}$, reward $r_t$
-        st_, rt, done, info = self.env.step()
+        st_, _, done, info = self.env.step()
+        # take first state as achieved goal, take last state as desired goal
+        rt = self.reward(self.states()[0],self.states()[-1])
 
         # cast to tensor
         st_ = torch.tensor(st_, dtype=torch.float,device=self.device)
@@ -225,10 +212,10 @@ class TD3(Policy):
                 p_tar = i_tar.named_parameters()
                 d_tar = dict(p_tar)
                 for name, param in p:
-                    d_tar[name].data = tau*param.data + (1-tau) * d_tar[name].data
+                    d_tar[name].data = polyak*param.data + (1-polyak) * d_tar[name].data
         
         # HER replace goal
-        if timeout and (not done):
+        if timeout:
             right = self.cnt % MEMORY_CAPACITY 
             recall_epi = torch.zeros((t_episode, self.memory.shape[1]))
             mem = self.memory.detach().clone()
@@ -239,16 +226,18 @@ class TD3(Policy):
             # replace goal pos with end effector pos
             s = recall_epi[:,:N_STATES]
             a = recall_epi[:,N_STATES:N_STATES+N_ACTIONS]
-            r = recall_epi[:,-N_STATES-1:-N_STATES]
+            r = torch.zeros((t_episode, 1))
             s_ = recall_epi[:,-N_STATES:]
             
-            end_effector = self.robot.get_end_effector_ids()[-1]
-            x1,y1,z1 = self.robot.get_link_world_positions(end_effector)
-            fake_goal = torch.tensor([[x1,y1,z1]])
+            fake_goal = torch.tensor(self.states()[0])
+            # make z pos of fake goal real
+            fake_goal[-1] = 0.05
+            assert len(fake_goal)==3
             
             s[:,-STATES_SHAPE[-1]:] = fake_goal
             s_[:,-STATES_SHAPE[-1]:] = fake_goal
-            r[0,0] = 0.
+            for i in range(t_episode):
+                r[i, 0] = self.reward(s_[i, :STATES_SHAPE[0]], fake_goal)
             recall_epi = torch.cat([s,a,r,s_],1)
             for i in range(t_episode):
                 self.cnt += 1
@@ -258,31 +247,52 @@ class TD3(Policy):
         return rt, done
         
     def update_actor(self, si):
+        assert si.shape == torch.Size([BATCH_SIZE, N_STATES])
         am = self.μ(si)
+        assert am.shape == torch.Size([BATCH_SIZE, N_ACTIONS])
         Qm = self.Q1(si, am)
+        assert Qm.shape == torch.Size([BATCH_SIZE, 1])
         lossμ = -torch.mean(Qm) + ACTION_L2 * torch.mean(am**2)
+        assert lossμ.shape == torch.Size([])
         lossμ.backward(retain_graph=True)
         self.optimμ.step()
         self.optimμ.zero_grad()
         self.optimQ1.zero_grad()
+        self.optimQ2.zero_grad()
         
     def update_critic(self,si,ai,ri,si_):
+        assert si.shape == torch.Size([BATCH_SIZE, N_STATES])
+        assert ai.shape == torch.Size([BATCH_SIZE, N_ACTIONS])
+        assert ri.shape == torch.Size([BATCH_SIZE, 1])
+        assert si_.shape == torch.Size([BATCH_SIZE, N_STATES])
         ai_ = self.μ_tar(si_).detach()
         ai_ += NOISE_EPSILON*torch.randn(ai_.shape,device=self.device)
+        assert ai_.shape == torch.Size([BATCH_SIZE, N_ACTIONS])
         Q1_ = self.Q1_tar(si_,ai_).detach()
         Q2_ = self.Q2_tar(si_,ai_).detach()
+        assert Q1_.shape == torch.Size([BATCH_SIZE, 1])
+        assert Q2_.shape == torch.Size([BATCH_SIZE, 1])
         y = ri + γ * torch.min(Q1_, Q2_)
+        assert y.shape == torch.Size([BATCH_SIZE, 1])
         
         Q1 = self.Q1(si, ai)
+        assert Q1.shape == torch.Size([BATCH_SIZE, 1])
         lossQ1 = torch.mean((y-Q1)**2)
+        assert lossQ1.shape == torch.Size([])
         lossQ1.backward()
         self.optimQ1.step()
+        self.optimμ.zero_grad()
         self.optimQ1.zero_grad()
+        self.optimQ2.zero_grad()
         
         Q2 = self.Q2(si, ai)
+        assert Q2.shape == torch.Size([BATCH_SIZE, 1])
         lossQ2 = torch.mean((y-Q2)**2)
+        assert lossQ2.shape == torch.Size([])
         lossQ2.backward()
         self.optimQ2.step()
+        self.optimμ.zero_grad()
+        self.optimQ1.zero_grad()
         self.optimQ2.zero_grad()
 
     def save(self, filename):
@@ -303,30 +313,26 @@ def success(reward):
     return reward >= -0.5
 
 if __name__=="__main__":
-    sim = prl.simulators.Bullet(render=False)
+    sim = prl.simulators.Bullet(render=True)
     world = prl.worlds.BasicWorld(sim)
     box = world.load_box(position=(0.5,0,0.2),dimensions=(0.1,0.1,0.1),mass=0.1,color=[0,0,1,1])
     manipulator = world.load_robot('wam')
     end_effector = manipulator.get_end_effector_ids()[-1]
-    states = LinkWorldPositionState(manipulator) + JointPositionState(manipulator) + JointVelocityState(manipulator) + PositionState(box,world)
+    states = LinkWorldPositionState(manipulator, link_ids=end_effector) + JointPositionState(manipulator) + JointVelocityState(manipulator) + PositionState(box,world)
     STATES_SHAPE = [i.shape[0] for i in states()]
     print(STATES_SHAPE)
     N_STATES = sum(STATES_SHAPE)
     action = JointPositionChangeAction(manipulator, kp=manipulator.kp, kd=manipulator.kd)
-    r_cond = HasTouchedCondition(manipulator,box,world,0.5)
-    t_cond = HasTouchedCondition(manipulator,box,world,0.5)
-    reward = TerminalReward(r_cond,subreward=-1,final_reward=0)
-    #reward = ApproachBoxReward(manipulator,box,world)
-    env = Env(world, states, rewards=reward,actions=action,terminal_conditions=t_cond)
+    env = Env(world, states,actions=action)
     
     env.reset()
-    td3 = TD3(env, states, action,manipulator, "cuda")
+    td3 = TD3(env, states, action,manipulator,touched_reward, "cuda")
     
     #td3.load("td3.ckpt")
     num_episodes = 30000
     save_freq = 200
     
-    n_samples = 100
+    n_samples = 20
     n_success = 0
     s_reward = 0
     tik = time.time()
@@ -339,16 +345,14 @@ if __name__=="__main__":
         for t in range(t_episode):
             timeout = t==t_episode-1
             reward, done = td3.learn(timeout)
-            if done:
-                break
-        n_success += 1 if done else 0
-        s_reward += reward
+            s_reward += reward
+        n_success += 1 if success(reward) else 0
         #print("SUCCESS" if done else "FAIL",flush=True)
         
         # collect statistics of #n_samples results
         if i%n_samples==0:
             tok = time.time()
-            print("{}\tSuc rate: {:.2f}\tAvg reward: {:.2f}\tTime: {:.1f}".format(int(i/n_samples),n_success/n_samples,s_reward/n_samples,tok-tik),flush=True)
+            print("Epoch {}\tSuc rate: {:.2f}\tAvg reward: {:.2f}\tTime: {:.1f}".format(int(i/n_samples),n_success/n_samples,s_reward/n_samples,tok-tik),flush=True)
             n_success = 0
             s_reward = 0
             tik = time.time()
