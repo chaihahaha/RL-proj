dd406742f7949da8de9f0c4757e26fe0d56e9c76
柴士童 <chaihahaha@qq.com>
2020-10-27 20:48:13 +0800

    all the same with baseline?

--- mujoco_td3.py
+++ mujoco_td3.py
@@ -7,16 +7,17 @@ import numpy as np
 import time
 from itertools import count
 
-
+NOISE_EPSILON = 0.1
 MEMORY_CAPACITY = 5120
 EPSILON = 0.1
+RAND_EPSILON = 0.3
+ACTION_L2 = 0.5
 γ = 0.981
 LR = 1e-3
-BATCH_SIZE = 512
-TRAIN_FREQ = 10
-TARGET_REPLACE_ITER = TRAIN_FREQ *10
+BATCH_SIZE = 40
 tau = 0.9
 t_episode = 50
+n_batches = 40
 
 class μNet(nn.Module):
     def __init__(self, low, high):
@@ -106,10 +107,13 @@ class TD3(object):
         self.μ_tar.load_state_dict(self.μ.state_dict())
         self.Q1_tar.load_state_dict(self.Q1.state_dict())
         self.Q2_tar.load_state_dict(self.Q2.state_dict())
-        self.memory = torch.zeros((MEMORY_CAPACITY, N_STATES*2+N_ACTIONS+1),device=self.device)
-        self.cnt = 0
+        self.memory = torch.zeros((MEMORY_CAPACITY,t_episode,N_STATES*2+N_ACTIONS+2),device=self.device)
+        self.cnt_step = 0
+        self.cnt_epi = 0
 
     def learn(self, st):
+        lossμ, lossQ1, lossQ2 = 0,0,0
+    
         # get state $s_t$
         st = np.concatenate([st["observation"],st["achieved_goal"],st["desired_goal"]])
         st = torch.tensor(st).float().to(self.device).unsqueeze(0)
@@ -119,7 +123,10 @@ class TD3(object):
         at += EPSILON*torch.randn(at.shape,device=self.device)
         
         # cast to numpy
-        at_np = (at.data.cpu()).numpy()[0]
+        if np.random.uniform() < RAND_EPSILON:
+            at_np = self.env.action_space.sample()
+        else:
+            at_np = (at.data.cpu()).numpy()[0]
 
         # step in environment to get next state $s_{t+1}$, reward $r_t$
         st_dic, rt, done, info = self.env.step(at_np)
@@ -127,33 +134,32 @@ class TD3(object):
         st_ = torch.tensor(st_).float().to(self.device).unsqueeze(0)
 
         # keep (st, at, rt, st_) in memory
-        index = self.cnt % MEMORY_CAPACITY
-        self.memory[index,:N_STATES] = st.detach()
-        self.memory[index,N_STATES:N_STATES+N_ACTIONS] = at.detach()
-        self.memory[index,-N_STATES-1] = torch.tensor(rt)
-        self.memory[index,-N_STATES:] = st_
+        index = self.cnt_epi % MEMORY_CAPACITY
+        t_step = self.cnt_step % t_episode
+        self.memory[index,t_step,:N_STATES] = st.detach()
+        self.memory[index,t_step,N_STATES:N_STATES+N_ACTIONS] = torch.tensor(at_np)
+        self.memory[index,t_step,-N_STATES-2] = torch.tensor(rt)
+        mask = 0. if done else γ
+        self.memory[index,t_step,-N_STATES-1] = mask
+        self.memory[index,t_step,-N_STATES:] = st_.detach()
         
-        if (self.cnt + 1) % TRAIN_FREQ == 0:
-            # randomly sample from memory
-            right = self.cnt+1 if self.cnt<MEMORY_CAPACITY else MEMORY_CAPACITY
-            random_index = np.random.choice(right, BATCH_SIZE)
-            
-            s = self.memory[random_index,:N_STATES]
-            a = self.memory[random_index,N_STATES:N_STATES+N_ACTIONS]
-            r = self.memory[random_index,-N_STATES-1:-N_STATES]
-            s_ = self.memory[random_index,-N_STATES:]
-            
-            self.update_critic(s,a,r,s_)
+        if done:
+            for i in range(n_batches):
+                # randomly sample from memory
+                right = self.cnt_epi+1 if self.cnt_epi<MEMORY_CAPACITY else MEMORY_CAPACITY
+                random_index = np.random.choice(right, BATCH_SIZE)
+                s = self.memory[random_index,:,:N_STATES].flatten(0,1)
+                a = self.memory[random_index,:,N_STATES:N_STATES+N_ACTIONS].flatten(0,1)
+                r = self.memory[random_index,:,-N_STATES-2:-N_STATES-1].flatten(0,1)
+                mask = self.memory[random_index,:,-N_STATES-1:-N_STATES].flatten(0,1)
+                s_ = self.memory[random_index,:,-N_STATES:].flatten(0,1)
+                
+                lq1, lq2 = self.update_critic(s,a,r,mask,s_)
+                lossQ1 += lq1
+                lossQ2 += lq2
+                
+                lossμ += self.update_actor(s)
         
-        # update target network
-        if  (self.cnt+1) % TARGET_REPLACE_ITER == 0:
-            # randomly sample from memory
-            right = self.cnt+1 if self.cnt<MEMORY_CAPACITY else MEMORY_CAPACITY
-            random_index = np.random.choice(right, BATCH_SIZE)
-            s = self.memory[random_index,:N_STATES]
-            self.update_actor(s)
-            
-            # update target network
             update_pairs = [(self.μ, self.μ_tar), (self.Q1, self.Q1_tar), (self.Q2, self.Q2_tar)]
             for i, i_tar in update_pairs:
                 p = i.named_parameters()
@@ -162,53 +168,60 @@ class TD3(object):
                 for name, param in p:
                     d_tar[name].data = tau*param.data + (1-tau) * d_tar[name].data
         
-        # HER replace goal
-        if done:
-            right = self.cnt % MEMORY_CAPACITY 
-            recall_epi = torch.zeros((t_episode, N_STATES*2+N_ACTIONS+1),device=self.device)
-            
-            for i in range(t_episode):
-                recall_epi[i] = self.memory[(right-i) % MEMORY_CAPACITY]
+            # HER replace goal
+            recall_epi = self.memory[self.cnt_epi % MEMORY_CAPACITY]
 
             # replace goal pos with end effector pos
             fake_goal = recall_epi[0,N_STATES-6:N_STATES-3].clone()
             recall_epi[:,N_STATES-3:N_STATES] = fake_goal
             recall_epi[:,-3:] = fake_goal
             for i in range(t_episode):
-                recall_epi[i,-N_STATES-1] = torch.tensor(self.env.compute_reward(achieved_goal=recall_epi[i,-6:-3].cpu(), desired_goal=fake_goal.cpu(), info=info))
-            for i in range(t_episode):
-                self.cnt += 1
-                self.memory[self.cnt % MEMORY_CAPACITY] = recall_epi[i]
+                recall_epi[i,-N_STATES-2] = torch.tensor(self.env.compute_reward(achieved_goal=recall_epi[i,-6:-3].cpu(), desired_goal=fake_goal.cpu(), info=info))    
+            self.cnt_epi += 1
+            self.memory[self.cnt_epi % MEMORY_CAPACITY] = recall_epi[i].detach()
+            self.cnt_epi += 1
             
-        self.cnt += 1
-        return st_dic, rt, done, info
+        self.cnt_step += 1
+        return st_dic, rt, done, info, lossμ/n_batches, lossQ1/n_batches, lossQ2/n_batches
         
     def update_actor(self, si):
-        Qm = self.Q1(si, self.μ(si))
-        lossμ = -torch.mean(Qm)
+        ai = self.μ(si)
+        Q1 = self.Q1(si, ai)
+        lossμ = -torch.mean(Q1) + ACTION_L2 * torch.mean(ai**2)
         lossμ.backward()
         self.optimμ.step()
         self.optimμ.zero_grad()
         self.optimQ1.zero_grad()
+        self.optimQ2.zero_grad()
+        return lossμ.item()
         
-    def update_critic(self,si,ai,ri,si_):
-        ai_ = self.μ_tar(si_).detach()
-        ai_ += EPSILON*torch.randn(ai_.shape,device=self.device)
-        Q1_ = self.Q1_tar(si_,ai_).detach()
-        Q2_ = self.Q2_tar(si_,ai_).detach()
-        y = ri + γ * torch.min(Q1_, Q2_)
         
-        Q1 = self.Q1(si, ai)
+    def update_critic(self,si,ai,ri,mask,si_):
+        with torch.no_grad():
+            ai_ = self.μ_tar(si_)
+            ai_ += NOISE_EPSILON*torch.randn(ai_.shape,device=self.device)
+        
+            Q1_ = self.Q1_tar(si_,ai_)
+            Q2_ = self.Q2_tar(si_,ai_)
+        
+        y = ri + mask * torch.min(Q1_, Q2_)
+
+        Q1 = self.Q1(si, ai) 
         lossQ1 = torch.mean((y-Q1)**2)
         lossQ1.backward()
         self.optimQ1.step()
+        self.optimμ.zero_grad()
         self.optimQ1.zero_grad()
+        self.optimQ2.zero_grad()
         
         Q2 = self.Q2(si, ai)
         lossQ2 = torch.mean((y-Q2)**2)
         lossQ2.backward()
         self.optimQ2.step()
-        self.optimQ2.zero_grad()   
+        self.optimμ.zero_grad()
+        self.optimQ1.zero_grad()
+        self.optimQ2.zero_grad()
+        return lossQ1.item(),lossQ2.item() 
     
     def save(self, filename):
         torch.save({'μ':self.μ_tar.state_dict(),
@@ -238,30 +251,36 @@ if __name__=="__main__":
     num_episodes = 30000
     save_freq = 200
     
-    n_samples = 100
+    n_samples = 10
     n_success = 0
     s_reward = 0
+    sum_lossμ = 0
+    sum_lossQ1 = 0
+    sum_lossQ2 = 0
     tik = time.time()
     for i in range(1,num_episodes+1):
         s = env.reset()
         # run an episode
         done = False
-        flag_success = False
         while not done:
             #env.render()
-            s, reward, done, info = td3.learn(s)
+            s, reward, done, info, lossμ, lossQ1, lossQ2 = td3.learn(s)
+            sum_lossμ += lossμ
+            sum_lossQ1 += lossQ1
+            sum_lossQ2 += lossQ2
             s_reward += reward
-            if info['is_success']:
-                flag_success = True
             if done:
                 break
-        n_success += 1 if flag_success else 0
+        n_success += 1 if info['is_success'] else 0
         #print("SUCCESS" if done else "FAIL",flush=True)
         
         # collect statistics of #n_samples results
         if i%n_samples==0:
             tok = time.time()
-            print("{}\tSuc rate: {:.2f}\tAvg reward: {:.2f}\tTime: {:.1f}".format(int(i/n_samples),n_success/n_samples,s_reward/n_samples,tok-tik),flush=True)
+            print("Epoch {}\tSuc rate: {:.2f}\tAvg reward: {:.2f}\tLossμ:{:.3f}\tLossQ1:{:.3f}\tLossQ2:{:.3f}\tTime: {:.1f}".format(int(i/n_samples),n_success/n_samples,s_reward/n_samples,sum_lossμ/n_samples, sum_lossQ1/n_samples, sum_lossQ2/n_samples, tok-tik),flush=True)
+            sum_lossμ = 0
+            sum_lossQ1 = 0
+            sum_lossQ2 = 0
             n_success = 0
             s_reward = 0
             tik = time.time()
