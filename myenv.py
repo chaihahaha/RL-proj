513491ae153e5cab703cd706c53c2021fa564ad7
柴士童 <chaihahaha@qq.com>
2020-10-29 09:35:42 +0800

    fix big polyak bug

--- myenv.py
+++ myenv.py
@@ -4,6 +4,7 @@ from pyrobolearn.policies import Policy
 from pyrobolearn.states.body_states import DistanceState, PositionState, VelocityState
 from pyrobolearn.states import JointPositionState, JointVelocityState, LinkWorldPositionState, LinkWorldVelocityState
 from pyrobolearn.actions import JointPositionChangeAction
+from pyrobolearn.terminal_conditions import TerminalCondition
 
 import torch
 import torch.nn as nn
@@ -12,18 +13,21 @@ import numpy as np
 import time
 from itertools import count
 
-N_ACTIONS = 15
-MEMORY_CAPACITY = int(1e3)
-NOISE_EPSILON = 0.1
+MEMORY_CAPACITY = int(5e3)
+NOISE_EPSILON = 0.2
+NOISE_CLIP = 0.5
 RAND_EPSILON = 0.3
 ACTION_L2 = 0.5
-γ = 0.99
-LR = 1e-3
-BATCH_SIZE = 128
-TARGET_REPLACE_IER = 20
-polyak = 0.05
-t_episode = 100
-
+LR = 3e-4
+BATCH_SIZE = 8
+EPI_BATCH_SIZE = 8
+TARGET_REPLACE_ITER = 2
+DELAY_ACTOR_ITER = 2
+polyak = 0.005
+t_episode = 64
+γ = 1-1/t_episode
+start_timesteps = 25e3
+        
 def picked_and_lifted_reward(box_pos):
     assert len(box_pos) == 3
     x,y,z = box_pos
@@ -70,9 +74,9 @@ class μNet(nn.Module):
     def forward(self, x):
         x = self.act(self.fc1(x))
         x = self.act(self.fc2(x))
-        x = self.act(self.fc3(x))
-        x = self.act(self.fc4(x))
-        x = self.act(self.fc5(x))
+        #x = self.act(self.fc3(x))
+        #x = self.act(self.fc4(x))
+        #x = self.act(self.fc5(x))
         x = self.out(x)
         x = self.tanh(x) * self.max_action
         return x
@@ -101,10 +105,10 @@ class QNet(nn.Module):
         x = torch.cat([x1,x2/self.max_action],dim=1)
         x = self.act(self.fc1(x))
         x = self.act(self.fc2(x))
-        x = self.act(self.fc3(x))
-        x = self.act(self.fc4(x))
-        x = self.act(self.fc5(x))
-        out = self.out(x)
+        #x = self.act(self.fc3(x))
+        #x = self.act(self.fc4(x))
+        #x = self.act(self.fc5(x))
+        out = self.tanh(self.out(x))/(1-γ)
         return out
 
 class TD3(Policy):
@@ -129,9 +133,8 @@ class TD3(Policy):
         self.optimQ1 = torch.optim.Adam(trainableQ1, lr=LR)
         trainableQ2 = list(filter(lambda p: p.requires_grad, self.Q2.parameters()))
         self.optimQ2 = torch.optim.Adam(trainableQ2, lr=LR)
-        with torch.no_grad():
-            self.μ_tar = μNet(self.max_action)
-            self.Q1_tar, self.Q2_tar = QNet(self.max_action), QNet(self.max_action)
+        self.μ_tar = μNet(self.max_action)
+        self.Q1_tar, self.Q2_tar = QNet(self.max_action), QNet(self.max_action)
         self.μ_tar.to(device)
         self.Q1_tar.to(self.device)
         self.Q2_tar.to(self.device)
@@ -142,28 +145,21 @@ class TD3(Policy):
         self.cnt_step = 0
         self.cnt_epi = 0
 
-    def learn(self, timeout):
+    def learn(self, done):
         lossμ, lossQ1, lossQ2 = 0,0,0
         
         # get state $s_t$
         st = self.states.vec_torch_data.float().to(self.device).unsqueeze(0)
-        # compute action with actor μ
-        at = self.μ(st).detach()
-        at += NOISE_EPSILON*torch.randn(at.shape,device=self.device)
-        #print(self.Q1(st,at).cpu()[0,0])
         
-        # cast to numpy
-        if np.random.uniform() < RAND_EPSILON:
-            at_np = self.env.action.space.sample()
-        else:
-            at_np = (at.data.cpu()).numpy()[0]
+        # get action $a_t$
+        at_np = self.get_action(st)
 
         # apply action
         self.set_action_data(at_np)
         self.actions()
         
         # step in environment to get next state $s_{t+1}$, reward $r_t$
-        st_, _, done, info = self.env.step()
+        st_, _, _, info = self.env.step()
         # take first state as achieved goal, take last state as desired goal
         rt = self.reward(self.states()[0],self.states()[-1])
 
@@ -171,31 +167,29 @@ class TD3(Policy):
         st_ = torch.tensor(st_, dtype=torch.float,device=self.device)
         
         # keep (st, at, rt, st_) in memory
-        index = self.cnt_epi % MEMORY_CAPACITY
-        t_step = self.cnt_step % t_episode
-        self.memory[index,t_step,:N_STATES] = st.detach()
-        self.memory[index,t_step,N_STATES:N_STATES+N_ACTIONS] = torch.tensor(at_np)
-        self.memory[index,t_step,-N_STATES-2] = torch.tensor(rt)
-        mask = 0. if timeout else γ
-        self.memory[index,t_step,-N_STATES-1] = mask
-        self.memory[index,t_step,-N_STATES:] = st_.detach()
-        if timeout:
+        self.store_transition(st, at_np, rt, 0. if done else γ, st_)
+        
+        if self.cnt_step>start_timesteps:
             # randomly sample from memory
-            right = self.cnt_epi+1 if self.cnt_epi<MEMORY_CAPACITY else MEMORY_CAPACITY
-            random_index = np.random.choice(right, BATCH_SIZE)
-            s = self.memory[random_index,:,:N_STATES].flatten(0,1)
-            a = self.memory[random_index,:,N_STATES:N_STATES+N_ACTIONS].flatten(0,1)
-            r = self.memory[random_index,:,-N_STATES-2:-N_STATES-1].flatten(0,1)
-            mask = self.memory[random_index,:,-N_STATES-1:-N_STATES].flatten(0,1)
-            s_ = self.memory[random_index,:,-N_STATES:].flatten(0,1)
+            right = min(self.cnt_epi , MEMORY_CAPACITY)
+            random_epi_index = np.random.choice(right, EPI_BATCH_SIZE)
+            random_t_index = np.random.choice(t_episode, BATCH_SIZE)
+            mem = self.memory[random_epi_index, :]
+            mem = mem[:, random_t_index].flatten(0,1)
+            s = mem[:,:N_STATES]
+            a = mem[:,N_STATES:N_STATES+N_ACTIONS]
+            r = mem[:,-N_STATES-2:-N_STATES-1]
+            mask = mem[:,-N_STATES-1:-N_STATES]
+            s_ = mem[:,-N_STATES:]
                 
             lq1, lq2 = self.update_critic(s,a,r,mask,s_)
             lossQ1 += lq1
             lossQ2 += lq2
             
-            lossμ += self.update_actor(s)
+            if self.cnt_step % DELAY_ACTOR_ITER == 0:
+                lossμ += self.update_actor(s)
             
-            if self.cnt_epi % TARGET_REPLACE_IER == 0:
+            if self.cnt_step % TARGET_REPLACE_ITER == 0:
                 update_pairs = [(self.μ, self.μ_tar), (self.Q1, self.Q1_tar), (self.Q2, self.Q2_tar)]
                 for i, i_tar in update_pairs:
                     p = i.named_parameters()
@@ -203,7 +197,7 @@ class TD3(Policy):
                     d_tar = dict(p_tar)
                     for name, param in p:
                         d_tar[name].data = polyak*param.data + (1-polyak) * d_tar[name].data
-        
+        if done:
             # HER replace goal
             recall_epi = self.memory[self.cnt_epi % MEMORY_CAPACITY].clone()
             # replace goal pos with end effector pos
@@ -215,7 +209,7 @@ class TD3(Policy):
             
             fake_goal = torch.tensor(self.states()[0])
             # make z pos of fake goal real
-            fake_goal[-1] = 0.05
+            #fake_goal[-1] = 0.05
             assert fake_goal.shape==torch.Size([3,])
             
             s[:,-STATES_SHAPE[-1]:] = fake_goal
@@ -230,7 +224,30 @@ class TD3(Policy):
             
             
         self.cnt_step += 1
-        return rt, done, lossμ, lossQ1, lossQ2
+        return rt, lossμ, lossQ1, lossQ2
+        
+    def store_transition(self,st, at_np, rt, mask, st_):
+        index = self.cnt_epi % MEMORY_CAPACITY
+        t_step = self.cnt_step % t_episode
+        self.memory[index,t_step,:N_STATES] = st.detach()
+        self.memory[index,t_step,N_STATES:N_STATES+N_ACTIONS] = torch.tensor(at_np)
+        self.memory[index,t_step,-N_STATES-2] = torch.tensor(rt)
+        
+        self.memory[index,t_step,-N_STATES-1] = mask
+        self.memory[index,t_step,-N_STATES:] = st_.detach()
+        
+    def get_action(self, st):
+        # cast to numpy
+        if np.random.uniform() < RAND_EPSILON or self.cnt_step < start_timesteps:
+            at_np = self.env.action.space.sample()
+        else:
+            # compute action with actor μ
+            with torch.no_grad():
+                at = self.μ(st)
+            noise = (torch.randn_like(at)*NOISE_EPSILON).clamp(-NOISE_CLIP,NOISE_CLIP)
+            at = (at + noise).clamp(-self.max_action, self.max_action)
+            at_np = at.detach().cpu().numpy()[0]
+        return at_np
         
     def update_actor(self, si):
         ai = self.μ(si)
@@ -241,22 +258,17 @@ class TD3(Policy):
         self.optimμ.zero_grad()
         self.optimQ1.zero_grad()
         self.optimQ2.zero_grad()
-        
-        assert si.shape == torch.Size([BATCH_SIZE*t_episode, N_STATES])
-        assert ai.shape == torch.Size([BATCH_SIZE*t_episode, N_ACTIONS])
-        assert Q1.shape == torch.Size([BATCH_SIZE*t_episode, 1])
-        assert lossμ.shape == torch.Size([])
         return lossμ.item()
         
     def update_critic(self,si,ai,ri,mask,si_):
         with torch.no_grad():
             ai_ = self.μ_tar(si_)
-            ai_ += NOISE_EPSILON*torch.randn(ai_.shape,device=self.device)
+            noise = (torch.randn_like(ai_)*NOISE_EPSILON).clamp(-NOISE_CLIP,NOISE_CLIP)
+            ai_ = (ai_ + noise).clamp(-self.max_action, self.max_action)
         
             Q1_ = self.Q1_tar(si_,ai_)
             Q2_ = self.Q2_tar(si_,ai_)
-        
-        y = ri + mask * torch.min(Q1_, Q2_)
+            y = ri + mask * torch.min(Q1_, Q2_)
 
         Q1 = self.Q1(si, ai) 
         lossQ1 = torch.mean((y-Q1)**2)
@@ -273,19 +285,6 @@ class TD3(Policy):
         self.optimμ.zero_grad()
         self.optimQ1.zero_grad()
         self.optimQ2.zero_grad()
-        
-        assert si.shape == torch.Size([BATCH_SIZE*t_episode, N_STATES])
-        assert ai.shape == torch.Size([BATCH_SIZE*t_episode, N_ACTIONS])
-        assert ri.shape == torch.Size([BATCH_SIZE*t_episode, 1])
-        assert si_.shape == torch.Size([BATCH_SIZE*t_episode, N_STATES])
-        assert ai_.shape == torch.Size([BATCH_SIZE*t_episode, N_ACTIONS])
-        assert Q1_.shape == torch.Size([BATCH_SIZE*t_episode, 1])
-        assert Q2_.shape == torch.Size([BATCH_SIZE*t_episode, 1])
-        assert y.shape == torch.Size([BATCH_SIZE*t_episode, 1])
-        assert Q1.shape == torch.Size([BATCH_SIZE*t_episode, 1])
-        assert lossQ1.shape == torch.Size([])
-        assert Q2.shape == torch.Size([BATCH_SIZE*t_episode, 1])
-        assert lossQ2.shape == torch.Size([])
         return lossQ1.item(),lossQ2.item()
 
     def save(self, filename):
@@ -317,6 +316,8 @@ if __name__=="__main__":
     print(STATES_SHAPE)
     N_STATES = sum(STATES_SHAPE)
     action = JointPositionChangeAction(manipulator)
+    N_ACTIONS = action.space.sample().shape[0]
+    print(N_ACTIONS)
     env = Env(world, states,actions=action)
     
     env.reset()
@@ -327,7 +328,7 @@ if __name__=="__main__":
     num_episodes = 30000
     save_freq = 200
     
-    n_samples = 50
+    n_samples = 10
     n_success = 0
     s_reward = 0
     sum_lossμ = 0
@@ -341,8 +342,8 @@ if __name__=="__main__":
         
         # run an episode
         for t in range(t_episode):
-            timeout = (t==t_episode-1)
-            reward, done, lossμ, lossQ1, lossQ2 = td3.learn(timeout)
+            done = (t >= t_episode - 1)
+            reward, lossμ, lossQ1, lossQ2 = td3.learn(done)
             sum_lossμ += lossμ
             sum_lossQ1 += lossQ1
             sum_lossQ2 += lossQ2
@@ -353,7 +354,7 @@ if __name__=="__main__":
         # collect statistics of #n_samples results
         if i%n_samples==0:
             tok = time.time()
-            print("Epoch {}\tSuc rate: {:.2f}\tAvg reward: {:.2f}\tLossμ:{:.3f}\tLossQ1:{:.3f}\tLossQ2:{:.3f}\tTime: {:.1f}".format(int(i/n_samples),n_success/n_samples,s_reward/n_samples,sum_lossμ/n_samples, sum_lossQ1/n_samples, sum_lossQ2/n_samples, tok-tik),flush=True)
+            print("Epoch {}\tSuc rate: {:.2f}\tAvg reward: {:.2f}\tLossμ:{:.3f}\tLossQ1:{:.3f}\tLossQ2:{:.3f}\tTime: {:.1f}".format(int(i/n_samples),n_success/n_samples,s_reward/n_samples,sum_lossμ/DELAY_ACTOR_ITER/2/n_samples, sum_lossQ1/n_samples, sum_lossQ2/n_samples, tok-tik),flush=True)
             sum_lossμ = 0
             sum_lossQ1 = 0
             sum_lossQ2 = 0
